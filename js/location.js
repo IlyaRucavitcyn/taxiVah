1c2dffa6a7db6f37296165f8846cf4f0d4fbc6f3
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-02-17 17:47:32 +0300

    Openstreetmap map on the site is set #4

--- js/location.js
+++ js/location.js
@@ -8,3 +8,9 @@ L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
     subdomains: ['otile1','otile2','otile3','otile4']
 }).addTo( map );
+
+for ( var i=0; i < markers.length; ++i ) {
+   L.marker( [markers[i].lat, markers[i].lng] )
+      .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+      .addTo( map );
+}
