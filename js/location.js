a664784dfa01c6cead50bc3f25e71aa762139c02
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-02-24 11:36:12 +0300

    Google autocomplete field is placed & turned on

--- js/location.js
+++ js/location.js
@@ -15,17 +15,24 @@ function showPosition(pos) {
     centerCoordContainer.innerHTML = currentLocation['lat'].toFixed(6) + ' : ' + currentLocation['lng'].toFixed(6);
 }
 
-var map, currentLocation, marker;
+var map, currentLocation, marker, places;
 var centerCoordContainer = document.getElementById('output');
+var autocomplete;
+var countryRestrict = {'country': 'georgia'};
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15
   });
 
- map.addListener('center_changed', function () {
-   var center = map.getCenter();
-   centerCoordContainer.innerHTML = center.lat().toFixed(6) + ' : ' + center.lng().toFixed(6);
+  var input =  document.getElementById('pac-input');
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+  autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  map.addListener('center_changed', function () {
+    var center = map.getCenter();
+    centerCoordContainer.innerHTML = center.lat().toFixed(6) + ' : ' + center.lng().toFixed(6);
  })
 }
 
